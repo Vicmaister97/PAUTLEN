188edbb519a6dd891d98fd50f44fe269625c1a58
acarvajal1 <alfonso.carvajal@estudiante.uam.es>
2019-10-29 22:01:43 +0100

    funciones

--- p1/temp.c
+++ p1/temp.c
@@ -28,3 +28,70 @@ void escribir_elemento_vector(FILE * fpasm,char * nombre_vector, int tam_max, in
   // ahora solo queda meter la direccion del elemento indexado en la pila
   fprintf(fpasm, "\tpush dword eax\n");
 }
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+void declararFuncion(FILE * fd_asm, char * nombre_funcion, int num_var_loc){
+  // etiqueta del inicio de la funcion es "_" seguido por el nombre de la funcion y ":"
+  fprintf(fpasm, "\t_%s:\n", nombre_funcion);
+  // guardar en la pila el registro ebp --> puntero base para localizar en la pila
+  // los args y variables locales de la funcion
+  fprintf(fpasm, "\tpush ebp\n");
+  // copiamos en ebp el valor de puntero de pila (esp) para poder gestionar
+  // acceso a los parametros y variables locales
+  fprintf(fpasm, "\tmov ebp, esp\n");
+  // reservamos espacio en la pila para las variables locales de la funcion
+  // cada variable local son 4 bytes
+  fprintf(fpasm, "\tsub esp, %d\n", 4*num_var_loc);
+
+}
+
+void retornarFuncion(FILE * fd_asm, int es_variable){
+  fprintf(fpasm, "\tpop dword eax\n");
+  // si es_variable == 1 entonces lo que esta en la cima de la pila es una variable o algo equivalente
+  // y queremos el contenido de lo que esta guardado en esa direccion
+  // el valor de retorno de la funcion se deja en eax
+  if(es_variable == 1){
+    fprintf(fpasm, "\tmov eax, [eax]\n");
+  }
+  // restauramos el puntero de la pila
+  fprintf(fpasm, "\tmov esp, ebp\n");
+  // sacamos ebp de la pila
+  fprintf(fpasm, "\tpop dword ebp\n");
+  // volvemos al programa llamante y saca de la pila la direccion de retorno
+  fprintf(fpasm, "\tret\n");
+}
+
+
+void escribirParametro(FILE* fpasm, int pos_parametro, int num_total_parametros){
+  int aux_ebp;
+  // cada argumento ocupa 4 bytes, por eso se multiplica por 4
+  // 1er argumento se corresponde con la posicion 0
+  aux_ebp = 4 * (1 + (num_total_parametros - pos_parametro));
+  // calculamos la direccion efectiva del parametro y la cargamos en eax
+  // al encontrarnos dentro de la funcion, ebp contiene el valor de puntero a pila (esp)
+  // donde se encuentran todos los parametros
+  fprintf(fpasm, "\tlea eax, [ebp + %d]\n", aux_ebp);
+  // insertamos en la pila eax, que contiene la direccion efectiva del parametro
+  fprintf(fpasm, "\tpush dword eax\n");
+}
+
+void escribirVariableLocal(FILE* fpasm, int posicion_variable_local){
+  int aux_ebp;
+  // cada argumento ocupa 4 bytes, por eso se multiplica por 4
+  // 1er argumento se corresponde con la posicion 1
+  aux_ebp = 4 * posicion_variable_local;
+  // calculamos la direccion efectiva del parametro y la cargamos en eax
+  // al encontrarnos dentro de la funcion, ebp contiene el valor de puntero a pila (esp)
+  // donde se encuentran todos los argumentos
+  fprintf(fpasm, "\tlea eax, [ebp - %d]\n", aux_ebp);
+  // insertamos en la pila eax, que contiene la direccion efectiva del parametro
+  fprintf(fpasm, "\tpush dword eax\n");
+}
