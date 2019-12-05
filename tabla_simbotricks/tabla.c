7db6198a0e6d08e0df0acd8a9f95838822f8e330
VKGit <victorgarciacarrera@gmail.com>
2019-12-05 03:02:03 +0100

    postmerge v2.0 tabla.c v1.0 prueba_tabla

--- tabla_simbotricks/tabla.c
+++ tabla_simbotricks/tabla.c
@@ -8,13 +8,296 @@
 **
 *********************************************************/
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tabla.h"
 
 
-/*********************
+/*******************************************************
+* METODOS DE LA TABLA HASH *
+*******************************************************/
+
+struct _SIMBOLO{
+    char *identificador;                /* identificador */
+    CATEGORIA_SIMBOLO cat_simbolo;      /* categoría del simbolo */
+    TIPO tipo;                          /* tipo */
+    CATEGORIA categoria;                /* categoria de la variable */
+    int valor;                          /* valor si escalar */
+    int longitud;                       /* longitud si vector */
+    int num_parametros;                 /* número de parámetros si función */
+    int posicion;                       /* posición en llamada a función si parámetro, posición de declaración si variable local de función */
+    int num_var_locales;                /* número de variables locales si función */
+};
+
+struct _listaSimbolo{
+  SIMBOLO **lista;
+  int len;
+};
+
+struct _HASH_TABLE{
+  listaSimbolo **hash_array;
+  int num_items;
+};
+
+SIMBOLO *newSimbolo(char *identificador, int valor){
+  SIMBOLO *s;
+  s = (SIMBOLO *)malloc(sizeof(SIMBOLO));
+  if(s == NULL)
+    return NULL;
+  s->identificador = (char *)malloc(sizeof(identificador));
+  strcpy(s->identificador, identificador);
+  s->valor = valor;
+  return s;
+}
+
+void setCategoriaSimbolo(SIMBOLO *s, CATEGORIA_SIMBOLO c){
+  if(s)
+    s->cat_simbolo = c;
+}
+
+void setTipo(SIMBOLO *s, TIPO t){
+  if(s)
+    s->tipo = t;
+}
+
+void setCategoria(SIMBOLO *s, CATEGORIA c){
+  if(s)
+    s->categoria = c;
+}
+
+void setValor(SIMBOLO *s, int v){
+  if(s != NULL)
+    s->valor = v;
+}
+
+void setLongitud(SIMBOLO *s, int l){
+  if(s != NULL)
+    s->longitud = l;
+}
+
+void setNum_parametros(SIMBOLO *s, int n){
+  if(s != NULL)
+    s->num_parametros = n;
+}
+
+void setPosicion(SIMBOLO *s, int p){
+  if(s != NULL)
+    s->posicion = p;
+}
+
+void setNum_var_locales(SIMBOLO *s, int n){
+  if(s != NULL)
+    s->num_var_locales = n;
+}
+
+void freeSimbolo(SIMBOLO *s){
+  if(s){
+    if(s->identificador)
+      free(s->identificador);
+    free(s);
+  }
+}
+
+char *getIdentificador(SIMBOLO *s){
+  if(s)
+    return s->identificador;
+}
+
+CATEGORIA_SIMBOLO CategoriaSimbolo(SIMBOLO *s){
+  if(s)
+    return s->cat_simbolo;
+}
+
+TIPO getTipo(SIMBOLO *s){
+  if(s)
+    return s->tipo;
+}
+
+CATEGORIA getCategoria(SIMBOLO *s){
+  if(s)
+    return s->categoria;
+}
+
+int getValor(SIMBOLO *s){
+  if(s)
+    return s->valor;
+}
+
+int getLongitud(SIMBOLO *s){
+  if(s)
+    return s->longitud;
+}
+
+int getNum_parametros(SIMBOLO *s){
+  if(s)
+    return s->num_parametros;
+}
+
+int getPosicion(SIMBOLO *s){
+  if(s)
+    return s->posicion;
+}
+
+int getNum_var_locales(SIMBOLO *s){
+  if(s)
+    return s->num_var_locales;
+}
+
+
+listaSimbolo *newListaSimbolo(){
+  listaSimbolo *l;
+  l = (listaSimbolo *)malloc(sizeof(listaSimbolo));
+  if(l == NULL)
+    return NULL;
+  l->lista = NULL;
+  l->len = 0;
+}
+void insertaSimboloLista(listaSimbolo *l, SIMBOLO *s){
+  if(l == NULL || s == NULL)
+    return;
+  l->lista = (SIMBOLO **)realloc(l->lista, (l->len + 1) * sizeof(SIMBOLO *));
+  *(l->lista + l->len) = s;
+  l->len++;
+}
+
+int isSimboloEnLista(listaSimbolo *l, char *s){
+  int i;
+  if(l == NULL || s == NULL)
+    return ERR;
+  for(i=0; i<l->len; i++)
+    if(strcmp(s, (*(l->lista + i))->identificador) == 0)
+      return i;
+
+  return FALSE;
+}
+
+void freeListaSimbolo(listaSimbolo *l){
+  int i;
+  if(l != NULL){
+    if(l->lista != NULL){
+      for(i=0; i<l->len; i++)
+        freeSimbolo(*(l->lista + i));
+      free(l->lista);
+    }
+    free(l);
+  }
+}
+
+
+HASH_TABLE *newHashTable(){
+  HASH_TABLE *h;
+  int i;
+  h = (HASH_TABLE *)malloc(sizeof(HASH_TABLE));
+  if(h == NULL)
+    return NULL;
+  h->hash_array = (listaSimbolo **)malloc(MAX_HASH*sizeof(listaSimbolo *));
+  if(h->hash_array == NULL){
+    free(h);
+    return NULL;
+  }
+  for(i=0; i<MAX_HASH; i++)
+    *(h->hash_array + i) = newListaSimbolo();
+
+  h->num_items = 0;
+  return h;
+}
+
+void freeHashTable(HASH_TABLE *h){
+  int i;
+  if(h){
+    if(h->hash_array){
+      for(i=0; i<MAX_HASH; i++)
+        freeListaSimbolo(*(h->hash_array + i));
+      free(h->hash_array);
+    }
+    free(h);
+  }
+}
+
+int hashCode(char *s){
+  int len, code, i;
+  if(s){
+    len = strlen(s);
+    code = 1;
+    for(i=0; i<len; i++){
+      code = code**(s + i);
+      code = code%MAX_HASH;
+    }
+    return code;
+  }
+  return ERR;
+}
+
+int insertarSimbolo(HASH_TABLE *h, SIMBOLO *s){
+  int i, hash;
+  listaSimbolo *hash_item;
+  if(h == NULL || s == NULL)
+    return ERR;
+
+  hash = hashCode(s->identificador);
+  hash_item = *(h->hash_array + hash);
+  /*Si tiene un item hay que ver si el que vamos a meter ya existe*/
+  if(0 < hash_item->len)
+    if(isSimboloEnLista(hash_item, s->identificador) != FALSE)
+      return ERR;
+
+  insertaSimboloLista(hash_item, s);
+  return TRUE;
+}
+
+SIMBOLO *buscarSimbolo(HASH_TABLE *h, char *s){
+  int hash, pos;
+  listaSimbolo *hash_item;
+  if(h == NULL || s == NULL)
+    return NULL;
+
+
+  hash = hashCode(s);
+  hash_item = *(h->hash_array + hash);
+  pos = isSimboloEnLista(hash_item, s);
+  if(pos != FALSE)
+    return *(hash_item->lista + pos);
+
+  return NULL;
+}
+
+void printSimbolo(SIMBOLO *s){
+  if(s){
+    printf("SIMBOLO: %s", s->identificador);
+  }
+}
+void printLista(listaSimbolo *l){
+  int i, len;
+  if(l){
+    len = l->len;
+    if(len <= 0)
+      printf("VACIA");
+    else{
+      printf("ITEM: ");
+      for(i=0; i<len; i++){
+        printf(" %d--> ", i);
+        printSimbolo(*(l->lista + i));
+      }
+    }
+  }
+}
+
+void printHashTable(HASH_TABLE *h){
+  int i;
+  if(h){
+    printf("HASH TABLE:\n");
+    for(i=0; i<MAX_HASH; i++){
+      printLista(*(h->hash_array + i));
+      printf("\n");
+    }
+  }
+}
+
+
+/*******************************************************
 * METODOS DE MODIFICACIÓN DE LA TABLA DE SÍMBOLOS (TS) *
-**********************/
+*******************************************************/
 
 // Declarar una variable de ámbito global
 int DeclararGlobal(HASH_TABLE *TGLOBAL, char *id, int desc_id){
@@ -85,84 +368,3 @@ int DeclararFuncion(HASH_TABLE *TGLOBAL, HASH_TABLE *TLOCAL, char *id, int desc_
         return TRUE;
     }
 }
-
-
-
-/*********************
-* METODOS PARA EN ANALIZADOR SINTÁCTICO *
-**********************/
-
-/*
-// Comprueba si el tipo de la expresión coincide con el tipo de retorno de la función
-void TS_CheckReturn(atributos expr, atributos* res){
-    int index = currentFunction;
-
-    if (index > -1) {
-        if (expr.type != TS[index].type) {
-            printf("RETURN ERR[line %d]: Return type not equal to function type. %d\n", line, expr.type);
-            return;
-        }
-        res->type = expr.type;
-        return;
-    }
-    else {
-        printf("RETURN ERR[line %d]: Result not declared into function.\n", line);
-        return;
-    }
-
-}
-
-// Devuelve en res la variable en la tabla de símbolos TS del identificador id si lo encuentra definido
-void TS_GetId(atributos id, atributos* res){
-    int index = TS_FindByID(id);
-    if(index == -1) {       // No es ninguna variable guardada en la TS
-        //if(TS[index].entry != FUNCTION)
-        printf("\nSEARCH ERR[line %d]: Id not found %s.\n", line, id.name);
-    }
-    else {
-        res->name = strdup(TS[index].name);
-        res->type = TS[index].type;
-    }
-}
-
-int TSGetId(atributos id){
-    int index = TS_FindByID(id);
-    if(index == -1) {       // No es ninguna variable guardada en la TS
-        printf("%s %i\n", id.name, id.type);
-        //if(id.type > 9){     // Si no tiene un tipo asignado, no es ni una constante, es una variable no declarada
-            printf("\nSEARCH ERR[line %d]: Id not found %s.\n", line, id.name);
-            return -1;
-        //}
-    }
-    else {
-        return TS[index].type;
-    }
-}
-
-// Realiza la comprobación de la llamada a una función
-void TS_FunctionCall(atributos* res){
-    if(checkFunct == -1) {
-        //currentFunction = -1;
-    }
-    else {
-        if (checkparam != TS[checkFunct].nParams) {
-            printf("ARGS ERR[line %d]: Number of params not valid.\n", line);
-        }
-        else {
-            res->name = strdup(TS[checkFunct].name);
-            res->type = TS[checkFunct].type;
-        }
-    }
-}
-
-// Realiza la comprobación de cada parámetro de una función
-void TS_CheckParam(atributos param){
-
-    checkparam += 1;
-    int formalparam = checkFunct + checkparam;
-
-    if(param.type != TS[formalparam].type) {
-        printf("ARGS ERR[line %d]: Param type of %s not valid (type %d).\n", line, param.name, param.type);
-        return;
-    }
-}*/
--- tabla_simbotricks/tabla.h
+++ tabla_simbotricks/tabla.h
@@ -1,9 +1,21 @@
-/******************************************************
-Nombre: tabla.h
-Descripcion: Definicion de simbolos para la tabla de simbolos
-******************************************************/
+/*********************************************************
+**
+** Fichero: tabla.h
+** Autores: Víctor García, Alfonso Carvajal
+** Contenido: Definiciones de la tabla de símbolos y su funcionalidad
+**            para el compilador a realizar en la asignatura de 
+**            Proyecto de Autómatas y Lenguajes
+**
+*********************************************************/
 #ifndef _TABLA_H
 #define _TABLA_H
+
+
+#define TRUE 1
+#define FALSE -1
+#define OK 1
+#define ERR -1
+#define MAX_HASH 20
 
 /* Categoría de un símbolo: variable, parámetro de función o función */
 typedef enum { VARIABLE, PARAMETRO, FUNCION } CATEGORIA_SIMBOLO;
@@ -15,45 +27,56 @@ typedef enum { ENTERO, BOOLEANO } TIPO;
 typedef enum { ESCALAR, VECTOR } CATEGORIA;
 
 /* Información de un símbolo */
-typedef struct {
-    char *identificador;                /* identificador */
-    CATEGORIA_SIMBOLO cat_simbolo;      /* categoría del simbolo */
-    TIPO tipo;                          /* tipo */
-    CATEGORIA categoria;                /* categoria de la variable */
-    int valor;                          /* valor si escalar */
-    int longitud;                       /* longitud si vector */
-    int num_parametros;                 /* número de parámetros si función */
-    int posicion;                       /* posición en llamada a función si parámetro, posición de declaración si variable local de función */
-    int num_var_locales;                /* número de variables locales si función */
-} SIMBOLO;
+typedef struct _SIMBOLO SIMBOLO;
+typedef struct _listaSimbolo listaSimbolo;
+typedef struct _HASH_TABLE HASH_TABLE;
 
-struct SIMBOLO *newSimbolo(char *identificador, TIPO t, CATEGORIA c);
-void setValor(struct SIMBOLO *s, int v);
-void setLongitud(struct SIMBOLO *s, int l);
-void setNum_parametros(struct SIMBOLO *s, int n);
-void setPosicion(struct SIMBOLO *s, int p);
-void setNum_var_locales(struct SIMBOLO *s, int n);
+SIMBOLO *newSimbolo(char *identificador, int valor);
+void setCategoriaSimbolo(SIMBOLO *s, CATEGORIA_SIMBOLO c);
+void setTipo(SIMBOLO *s, TIPO t);
+void setCategoria(SIMBOLO *s, CATEGORIA c);
+void setValor(SIMBOLO *s, int v);
+void setLongitud(SIMBOLO *s, int l);
+void setNum_parametros(SIMBOLO *s, int n);
+void setPosicion(SIMBOLO *s, int p);
+void setNum_var_locales(SIMBOLO *s, int n);
 
-void freeSimbolo(struct SIMBOLO *s);
+char *getIdentificador(SIMBOLO *s);
+CATEGORIA_SIMBOLO CategoriaSimbolo(SIMBOLO *s);
+TIPO getTipo(SIMBOLO *s);
+CATEGORIA getCategoria(SIMBOLO *s);
+int getValor(SIMBOLO *s);
+int getLongitud(SIMBOLO *s);
+int getNum_parametros(SIMBOLO *s);
+int getPosicion(SIMBOLO *s);
+int getNum_var_locales(SIMBOLO *s);
+
+void freeSimbolo(SIMBOLO *s);
 
 /* Aclaración: como los campos valor, longitud, num_parametros, posicion y num_var_locales
 son opcionales dependiendo de si el símbolo es variable (escalar o vector), parámetro o función,
 se pueden concentrar todos estos campos en sólo dos campos de tipo int (por ejemplo, info1 e info2)
 y gestionar en cada caso al guardar y recuperar los valores el dato que corresponde */
 
-typedef struct {
-  struct SIMBOLO **hash_array;
-  int num_items;
-  int max_items;
-} HASH_TABLE;
 
-struct HASH_TABLE *newHashTable(int max_items);
 
-void freeHashTable(struct HASH_TABLE *h);
+listaSimbolo *newListaSimbolo();
+void freeListaSimbolo(listaSimbolo *l);
+void insertaSimboloLista(listaSimbolo *l, SIMBOLO *s);
 
-int insertarSimbolo(struct HASH_TABLE *h, struct SIMBOLO s);
 
-struct SIMBOLO *buscarSimbolo(struct HASH_TABLE *h, char *identificador);
+
+HASH_TABLE *newHashTable();
+
+void freeHashTable(HASH_TABLE *h);
+
+int insertarSimbolo(HASH_TABLE *h, SIMBOLO *s);
+
+SIMBOLO *buscarSimbolo(HASH_TABLE *h, char *identificador);
+
+void printSimbolo(SIMBOLO *s);
+void printLista(listaSimbolo *l);
+void printHashTable(HASH_TABLE *h);
 
 
 #endif
